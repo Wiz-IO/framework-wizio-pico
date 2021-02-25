93a0ef3699b4447c5b848ef1e0e6de1c20a131f5
Georgi Angelov <the.wizarda@gmail.com>
2021-02-25 18:21:38 +0200

    Add files via upload

--- arduino/variants/pico/boards/pico.h
+++ arduino/variants/pico/boards/pico.h
@@ -14,19 +14,7 @@
 #ifndef _BOARDS_PICO_H
 #define _BOARDS_PICO_H
 
-#ifndef PICO_DEFAULT_UART
-#define PICO_DEFAULT_UART 0
-#endif
-
-#ifndef PICO_DEFAULT_UART_TX_PIN
-#define PICO_DEFAULT_UART_TX_PIN 0
-#endif
-
-#ifndef PICO_DEFAULT_UART_RX_PIN
-#define PICO_DEFAULT_UART_RX_PIN 1
-#endif
-
-#ifndef PICO_DEFAULT_LED_PIN
+#ifndef PICO_DEFAULT_LED_PIN /* need for tinyusb*/
 #define PICO_DEFAULT_LED_PIN 25
 #endif
 
@@ -36,17 +24,6 @@
 
 #ifndef PICO_FLASH_SIZE_BYTES
 #define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
-#endif
-
-// Drive high to force power supply into PWM mode (lower ripple on 3V3 at light loads)
-#define PICO_SMPS_MODE_PIN 23
-
-#ifndef PICO_FLOAT_SUPPORT_ROM_V1
-//#define PICO_FLOAT_SUPPORT_ROM_V1 1
-#endif
-
-#ifndef PICO_DOUBLE_SUPPORT_ROM_V1
-//#define PICO_DOUBLE_SUPPORT_ROM_V1 1
 #endif
 
 #endif
