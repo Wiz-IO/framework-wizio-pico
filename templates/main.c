1601718f6b78994f893fe97fa6d0d269704700b0
Wiz-IO <the.wizarda@gmail.com>
2021-03-06 10:31:03 +0200

    Signed-off-by: Wiz-IO <the.wizarda@gmail.com>

--- templates/main.c
+++ templates/main.c
@@ -1,36 +1,29 @@
-/**
-    PlatformIO - wizio-pico
-    https://github.com/Wiz-IO
-
+/*  
     Created : 01.01.2021
-    Author  : 
+    Author  : https://github.com/Wiz-IO/wizio-pico
 
-
-ADD TO platformio.ini (  Wiki: https://github.com/Wiz-IO/wizio-pico/wiki  )
-
-upload_port   = select PICO-DRIVE:/ or select HARD-DRIVE:/ to save the UF2 file ( example C:/ )
+upload_port   = PICO-DRIVE:/ 
 monitor_port  = COM* ... /dev/tty*
 monitor_speed = 115200
-;lib_deps = 
-;build_flags = 
 
  */
 
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED PICO_DEFAULT_LED_PIN
+
 int main(void)
 {  
     stdio_init_all();
     printf("\n\nHello World\n");
-    const uint LED_PIN = 25;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
     while (true)
     {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(50);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(50);
+        gpio_put(LED, 1);
+        sleep_ms(100);
+        gpio_put(LED, 0);
+        sleep_ms(100);
     }
 }
