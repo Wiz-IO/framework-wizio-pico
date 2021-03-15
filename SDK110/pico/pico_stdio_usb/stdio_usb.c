893f1a37d770073e09bf92390efb22dc6730ab56
Georgi Angelov <the.wizarda@gmail.com>
2021-03-15 10:18:18 +0200

    Update stdio_usb.c

--- SDK110/pico/pico_stdio_usb/stdio_usb.c
+++ SDK110/pico/pico_stdio_usb/stdio_usb.c
@@ -10,6 +10,7 @@
 #include "pico/time.h"
 #include "pico/stdio/driver.h"
 #include "pico/binary_info.h"
+#include "pico/mutex.h" //WizIO
 #include "hardware/irq.h"
 
 static_assert(PICO_STDIO_USB_LOW_PRIORITY_IRQ > RTC_IRQ, ""); // note RTC_IRQ is currently the last one
