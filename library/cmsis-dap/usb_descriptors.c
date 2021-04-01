4a19d40e16a5e3c7bdbc5b440b2a058ae420df5c
Georgi Angelov <the.wizarda@gmail.com>
2021-04-01 15:41:47 +0300

    Add files via upload

---
+++ library/cmsis-dap/usb_descriptors.c
@@ -0,0 +1,192 @@
+#include "tusb.h"
+#include "DAP.h"
+#include "pico/multicore.h"
+
+// String Descriptor Index
+enum
+{
+  STRID_LANGID = 0,
+  STRID_PRODUCT,
+  STRID_MANUFACTURER,
+  STRID_SERIAL,
+};
+
+//--------------------------------------------------------------------+
+// Device Descriptors
+//--------------------------------------------------------------------+
+#define _PID_MAP(itf, n) ((CFG_TUD_##itf) << (n))
+#define USB_PID (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4))
+
+tusb_desc_device_t const desc_device =
+    {
+        .bLength = sizeof(tusb_desc_device_t),
+        .bDescriptorType = TUSB_DESC_DEVICE,
+        .bcdUSB = 0x0200,
+        .bDeviceClass = 0x00,
+        .bDeviceSubClass = 0x00,
+        .bDeviceProtocol = 0x00,
+        .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+
+        .idVendor = 0xCafe,
+        .idProduct = USB_PID,
+        .bcdDevice = 0x0100,
+
+        .iManufacturer = STRID_MANUFACTURER,
+        .iProduct = STRID_PRODUCT,
+        .iSerialNumber = STRID_SERIAL,
+
+        .bNumConfigurations = 0x01};
+
+// Invoked when received GET DEVICE DESCRIPTOR
+// Application return pointer to descriptor
+uint8_t const *tud_descriptor_device_cb(void)
+{
+  return (uint8_t const *)&desc_device;
+}
+
+//--------------------------------------------------------------------+
+// HID Report Descriptor
+//--------------------------------------------------------------------+
+
+static uint8_t const desc_hid_report[] =
+    {
+        TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE)};
+
+// Invoked when received GET HID REPORT DESCRIPTOR
+// Application return pointer to descriptor
+// Descriptor contents must exist long enough for transfer to complete
+uint8_t const *tud_hid_descriptor_report_cb(void)
+{
+  return desc_hid_report;
+}
+
+//--------------------------------------------------------------------+
+// Configuration Descriptor
+//--------------------------------------------------------------------+
+
+enum
+{
+  ITF_NUM_HID,
+  ITF_NUM_TOTAL
+};
+
+#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_INOUT_DESC_LEN)
+
+#define EPNUM_HID 0x01
+
+uint8_t const desc_configuration[] =
+    {
+        // Config number, interface count, string index, total length, attribute, power in mA
+        TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+
+        // Interface number, string index, protocol, report descriptor len, EP In & Out address, size & polling interval
+        TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 0, HID_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, 0x80 | EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 1)};
+
+// Invoked when received GET CONFIGURATION DESCRIPTOR
+// Application return pointer to descriptor
+// Descriptor contents must exist long enough for transfer to complete
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
+{
+  (void)index; // for multiple configurations
+  return desc_configuration;
+}
+
+//--------------------------------------------------------------------+
+// String Descriptors
+//--------------------------------------------------------------------+
+
+char unique_id[17] = "0000000000000000";
+
+// array of pointer to string descriptors
+char const *string_desc_arr[] =
+    {
+        [STRID_LANGID] = (const char[]){0x09, 0x04}, // supported language is English (0x0409)
+        [STRID_PRODUCT] = "CMSIS-DAP",               // Product
+        [STRID_MANUFACTURER] = "pico-debug",         // Manufacturer
+        [STRID_SERIAL] = (const char *)unique_id,    // Serial
+};
+
+static uint16_t _desc_str[32];
+
+// Invoked when received GET STRING DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
+  (void)langid;
+
+  uint8_t chr_count;
+
+  if (index == 0)
+  {
+    memcpy(&_desc_str[1], string_desc_arr[0], 2);
+    chr_count = 1;
+  }
+  else
+  {
+    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+
+    if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
+      return NULL;
+
+    const char *str = string_desc_arr[index];
+
+    // Cap at max char
+    chr_count = strlen(str);
+    if (chr_count > 31)
+      chr_count = 31;
+
+    // Convert ASCII string into UTF-16
+    for (uint8_t i = 0; i < chr_count; i++)
+    {
+      _desc_str[1 + i] = str[i];
+    }
+  }
+
+  // first byte is length (including header), second byte is string type
+  _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
+
+  return _desc_str;
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
+{
+  // TODO not Implemented
+  (void)report_id;
+  (void)report_type;
+  (void)buffer;
+  (void)reqlen;
+  return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const *RxDataBuffer, uint16_t bufsize)
+{
+  static uint8_t TxDataBuffer[CFG_TUD_HID_EP_BUFSIZE];
+  uint32_t response_size = TU_MIN(CFG_TUD_HID_EP_BUFSIZE, bufsize);
+  DAP_ProcessCommand(RxDataBuffer, TxDataBuffer);
+  tud_hid_report(0, TxDataBuffer, response_size);
+}
+
+//--------------------------------------------------------------------+
+// RUN
+//--------------------------------------------------------------------+
+
+static void dap_main(void)
+{
+    DAP_Setup();
+    tusb_init();
+    while (true)
+        tud_task();
+}
+
+void dap_init(void)
+{
+    multicore_launch_core1(dap_main);
+}
