fd9dd1ddd98788acda0ba7cc1bed6f29035db599
Georgi Angelov <the.wizarda@gmail.com>
2021-05-02 13:45:19 +0300

    fix new[]

--- arduino/arduino/new.cpp
+++ arduino/arduino/new.cpp
@@ -1,44 +1,54 @@
-/*
-  Copyright (c) 2014 Arduino.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//      2021 Georgi Angelov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////////////
 
 #include <interface.h>
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wsized-deallocation"
+//#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Wsized-deallocation"
 
 void *operator new(size_t size)
 {
-  return malloc(size);
+  return std::malloc(size);
 }
 
 void *operator new[](size_t size)
 {
-  return malloc(size);
+  return std::malloc(size);
+}
+
+void operator delete(void *ptr, __unused std::size_t n) noexcept
+{
+  std::free(ptr);
+}
+
+void operator delete[](void *ptr, __unused std::size_t n) noexcept
+{
+  std::free(ptr);
 }
 
 void operator delete(void *ptr) noexcept
 {
-  free(ptr);
+  std::free(ptr);
 }
 
 void operator delete[](void *ptr) noexcept
 {
-  free(ptr);
+  std::free(ptr);
 }
 
-#pragma GCC diagnostic pop
+//#pragma GCC diagnostic pop
